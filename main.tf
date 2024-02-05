110a3f2328500b783bf2dd0fc66b3fe32e4383ea
Pernelle M <pernelle.mensah@gmail.com>
2024-02-04 21:21:09 -0500

    Formatted main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,44 @@
 terraform {
 
-/* providers */
+  /* providers */
 
-	required_providers {
-		aws = {
-			source = "hashicorp/aws"
-			version = "~> 4.16"
-		}
-	}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
-	required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"
 
-	provider aws {
-		region = "us-east-1"
-	}
+  provider "aws" {
+    region = "us-east-1"
+  }
 
-	/* instances */
+  /* instances */
 
-	resource "aws_instance" "wazuh_server" {
-		ami = "ami-0c7217cdde317cfec"
-		instance_type = "t2.xlarge"
-		security_groups = ["AWS_wazuh_sg"]
-		tags = {
-			Name = "WazuhServerInstance"
-		}
-		user_data = <<-EOL
+  resource "aws_instance" "wazuh_server" {
+    ami             = "ami-0c7217cdde317cfec"
+    instance_type   = "t2.xlarge"
+    security_groups = ["AWS_wazuh_sg"]
+    tags = {
+      Name = "WazuhServerInstance"
+    }
+    user_data = <<-EOL
 					  #!/bin/bash -xe
 					  apt update
 					  curl -sO https://packages.wazuh.com/4.7/wazuh-install.sh && bash ./wazuh-install.sh -a
 					  EOL
-	}
+  }
 
-	resource "aws_instance" "thehive_server" {
-		ami = "ami-0c7217cdde317cfec"
-		instance_type = "t2.xlarge"
-		security_groups = ["AWS_thehive_sg"]
-		tags = {
-			Name = "TheHiveServerInstance"
-		}
-		user_data = <<-EOL
+  resource "aws_instance" "thehive_server" {
+    ami             = "ami-0c7217cdde317cfec"
+    instance_type   = "t2.xlarge"
+    security_groups = ["AWS_thehive_sg"]
+    tags = {
+      Name = "TheHiveServerInstance"
+    }
+    user_data = <<-EOL
 					  #!/bin/bash -xe
 					  apt-get update
 					  apt-get install -y openjdk-8-jre-headless
@@ -54,115 +54,115 @@ terraform {
 					  apt install thehive4
 					  EOL
 
-	}
+  }
 
-	/* networking */
+  /* networking */
 
-	resource "aws_vpc" "soc_vpc" {
-		cidr_bloc = "10.5.0.0/24"
-		instance_tenancy = "default"
-		enable_dns_hostname = true
-		tags = {
-			Name = "soc_vpc"
-		}
-	}
+  resource "aws_vpc" "soc_vpc" {
+    cidr_bloc           = "10.5.0.0/24"
+    instance_tenancy    = "default"
+    enable_dns_hostname = true
+    tags = {
+      Name = "soc_vpc"
+    }
+  }
 
-	resource "aws_internet_gateway" "soc_gw" {
-	  	vpc_id = aws_vpc.soc_vpc.id
-	  	tags = {
-	    	Name = "soc gateway"
-	  	}
-	}
+  resource "aws_internet_gateway" "soc_gw" {
+    vpc_id = aws_vpc.soc_vpc.id
+    tags = {
+      Name = "soc gateway"
+    }
+  }
 
-	resource "aws_subnet" "soc_subnet" {
-	    vpc_id                  = aws_vpc.soc_vpc.id
-	    cidr_block              = "10.5.0.0/24"
-	    availability_zone       = "us-east-1a"
-	    map_public_ip_on_launch = true
-	    tags = {
-	        Name = "AWS_Soc subnet"
-	    }
-	}
+  resource "aws_subnet" "soc_subnet" {
+    vpc_id                  = aws_vpc.soc_vpc.id
+    cidr_block              = "10.5.0.0/24"
+    availability_zone       = "us-east-1a"
+    map_public_ip_on_launch = true
+    tags = {
+      Name = "AWS_Soc subnet"
+    }
+  }
 
-	resource "aws_route_table" "soc_rt" {
-	    vpc_id = aws_vpc.soc_vpc.id
-	    route {
-	        cidr_block = "0.0.0.0/0"
-	        gateway_id = aws_internet_gateway.soc_gw.id
-	    }
-	}
+  resource "aws_route_table" "soc_rt" {
+    vpc_id = aws_vpc.soc_vpc.id
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.soc_gw.id
+    }
+  }
 
-	resource "aws_route_table_association" "soc_public_rta" {
-	    subnet_id      = aws_subnet.soc_subnet.id
-	    route_table_id = aws_route_table.soc_rt.id
-	}
+  resource "aws_route_table_association" "soc_public_rta" {
+    subnet_id      = aws_subnet.soc_subnet.id
+    route_table_id = aws_route_table.soc_rt.id
+  }
 
-	/* security groups */
+  /* security groups */
 
-	resource "aws_security_group" "thehive_sg" {
-	    name        = "AWS_thehive_sg"
-	    description = "AWS_thehive_sg"
-	    vpc_id      = aws_vpc.soc_vpc.id
-	    ingress {
-	        from_port   = 22
-	        to_port     = 22
-	        protocol    = "tcp"
-	        cidr_blocks = ["0/32"]
-	    }
-	    tags = {
-	        Name = "AWS_thehive_sg"
-	    }
-	}
+  resource "aws_security_group" "thehive_sg" {
+    name        = "AWS_thehive_sg"
+    description = "AWS_thehive_sg"
+    vpc_id      = aws_vpc.soc_vpc.id
+    ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0/32"]
+    }
+    tags = {
+      Name = "AWS_thehive_sg"
+    }
+  }
 
-	resource "aws_security_group" "wazuh_sg" {
-	    name        = "AWS_wazuh_sg"
-	    description = "AWS_wazuh_sg"
-	    vpc_id      = aws_vpc.soc_vpc.id
-	    ingress {
-	        from_port   = 22
-	        to_port     = 22
-	        protocol    = "tcp"
-	        cidr_blocks = ["0/32"]
-	    }
-	    ingress {
-	        to_port     = 1514
-	        protocol    = "tcp"
-	        cidr_blocks = ["0/32"]
-	    }
-	    ingress {
-	        to_port     = 1515
-	        protocol    = "tcp"
-	        cidr_blocks = ["0/32"]
-	    }
-	    ingress {
-	        to_port     = 1516
-	        protocol    = "tcp"
-	        cidr_blocks = ["0/32"]
-	    }
-	    ingress {
-	        to_port     = 55000
-	        protocol    = "tcp"
-	        cidr_blocks = ["0/32"]
-	    }
-	    ingress {
-	        to_port     = 9200
-	        protocol    = "tcp"
-	        cidr_blocks = ["0/32"]
-	    }
-	    ingress {
-	        to_port     = 9300-9400
-	        protocol    = "tcp"
-	        cidr_blocks = ["0/32"]
-	    }
-	    ingress {
-	        to_port     = 443
-	        protocol    = "tcp"
-	        cidr_blocks = ["0/32"]
-	    }
-	    tags = {
-	        Name = "AWS_wazuh_sg"
-	    }
-	}
+  resource "aws_security_group" "wazuh_sg" {
+    name        = "AWS_wazuh_sg"
+    description = "AWS_wazuh_sg"
+    vpc_id      = aws_vpc.soc_vpc.id
+    ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0/32"]
+    }
+    ingress {
+      to_port     = 1514
+      protocol    = "tcp"
+      cidr_blocks = ["0/32"]
+    }
+    ingress {
+      to_port     = 1515
+      protocol    = "tcp"
+      cidr_blocks = ["0/32"]
+    }
+    ingress {
+      to_port     = 1516
+      protocol    = "tcp"
+      cidr_blocks = ["0/32"]
+    }
+    ingress {
+      to_port     = 55000
+      protocol    = "tcp"
+      cidr_blocks = ["0/32"]
+    }
+    ingress {
+      to_port     = 9200
+      protocol    = "tcp"
+      cidr_blocks = ["0/32"]
+    }
+    ingress {
+      to_port     = 9300 - 9400
+      protocol    = "tcp"
+      cidr_blocks = ["0/32"]
+    }
+    ingress {
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0/32"]
+    }
+    tags = {
+      Name = "AWS_wazuh_sg"
+    }
+  }
 
 
 }
