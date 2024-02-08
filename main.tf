b6594402b5f9981dab2be6ba4079c1ee51c137e3
Pernelle M <pernelle.mensah@gmail.com>
2024-02-08 17:49:03 -0500

    Fixing theHive ip address

--- main.tf
+++ main.tf
@@ -213,13 +213,13 @@ resource "aws_instance" "thehive_server" {
 					  systemctl start thehive
 					  systemctl enable thehive
 
-            curl -u ${var.default_thehive_user}:${var.default_thehive_password} -X POST -d  {"name": "myOrg", "description": "SOC automation"} http://${aws_instance.thehive_server.public_ip}:9000/api/v1/organisation 
+            curl -u ${var.default_thehive_user}:${var.default_thehive_password} -X POST -d  {"name": "myOrg", "description": "SOC automation"} http://127.0.0.1:9000/api/v1/organisation 
 
-            curl -u ${var.default_thehive_user}:${var.default_thehive_password} -X POST -d  {"login": "myorguseradmin@myorg.com", "name": "myOrgUserAdmin", "password":${var.myorg_thehive_user_admin_password}, "profile": "org-admin", "organisation": "myOrg"} http://${aws_instance.thehive_server.public_ip}:9000/api/v1/user
+            curl -u ${var.default_thehive_user}:${var.default_thehive_password} -X POST -d  {"login": "myorguseradmin@myorg.com", "name": "myOrgUserAdmin", "password":${var.myorg_thehive_user_admin_password}, "profile": "org-admin", "organisation": "myOrg"} http://127.0.0.1:9000/api/v1/user
 
-            curl -u ${var.default_thehive_user}:${var.default_thehive_password} -X POST -d  {"login": "myorguseranalyst@myorg.com", "name": "myOrgUserAnalyst", "password":${var.myorg_thehive_user_analyst_password}, "profile": "analyst", "organisation": "myOrg"} http://${aws_instance.thehive_server.public_ip}:9000/api/v1/user
+            curl -u ${var.default_thehive_user}:${var.default_thehive_password} -X POST -d  {"login": "myorguseranalyst@myorg.com", "name": "myOrgUserAnalyst", "password":${var.myorg_thehive_user_analyst_password}, "profile": "analyst", "organisation": "myOrg"} http://127.0.0.1:9000/api/v1/user
 
-            curl -u ${var.default_thehive_user}:${var.default_thehive_password} -X POST  http://${aws_instance.thehive_server.public_ip}:9000/api/v1/user/myOrgUser/key/renew
+            curl -u ${var.default_thehive_user}:${var.default_thehive_password} -X POST  http://127.0.0.1:9000/api/v1/user/myOrgUser/key/renew
 
 					  EOL
 
